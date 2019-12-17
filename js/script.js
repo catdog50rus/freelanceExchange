ac4f5dc50459c3316d43415ca434ce31dc691f64
catdog50rus <catdog50rus@gmail.com>
2019-12-17 16:01:46 +0300

    1d

---
+++ js/script.js
@@ -0,0 +1,63 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', () => {
+  const customer = document.getElementById('customer'),
+        freelancer = document.getElementById('freelancer'),
+        blockCustomer = document.getElementById('block-customer'),
+        blockFreelancer = document.getElementById('block-freelancer'),
+        blockChoice = document.getElementById('block-choice'),
+        btnExit = document.getElementById('btn-exit'),
+        formCustomer = document.getElementById('form-customer');
+  
+  const orders = [];
+
+  customer.addEventListener('click', () => {
+    blockFreelancer.style.display = 'none';
+    blockChoice.style.display = 'none';
+    blockCustomer.style.display = 'block';
+    btnExit.style.display = 'block';
+    
+  });
+
+  freelancer.addEventListener('click', () => {
+    blockCustomer.style.display = 'none';
+    blockChoice.style.display = 'none';
+    blockFreelancer.style.display = 'block';
+    btnExit.style.display = 'block';
+    
+  });
+
+
+  btnExit.addEventListener('click', () => {
+    blockFreelancer.style.display = 'none';
+    blockCustomer.style.display = 'none';
+    btnExit.style.display = 'none';
+    blockChoice.style.display = 'block';
+    
+    
+  });
+
+  formCustomer.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const obj = {};
+    for (const elem of formCustomer.elements){
+      if((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
+        (elem.type === 'radio' && elem.checked) ||
+        elem.tagName === 'TEXTAREA') {
+        
+        obj[elem.name] = elem.value;
+        
+        if(elem.type !== 'radio'){
+          elem.value = '';
+        }
+      }
+      
+
+    };
+    orders.push(obj);
+    console.log('orders: ', orders);
+  });
+
+
+
+  //console.log(customer, freelancer);
+});
