49de5560d780c71b93b879b3651eb8b91471e40f
catdog50rus <catdog50rus@gmail.com>
2019-12-19 14:05:02 +0300

    2day+dz

--- js/script.js
+++ js/script.js
@@ -6,9 +6,90 @@ document.addEventListener('DOMContentLoaded', () => {
         blockFreelancer = document.getElementById('block-freelancer'),
         blockChoice = document.getElementById('block-choice'),
         btnExit = document.getElementById('btn-exit'),
-        formCustomer = document.getElementById('form-customer');
+        formCustomer = document.getElementById('form-customer'),
+        ordersTable = document.getElementById('orders'),
+        modalOrder = document.getElementById('order_read'),
+        modalOrderActive = document.getElementById('order_active'),
+        formClose = document.querySelector('.close');
   
   const orders = [];
+
+  const renderOrders = () => {
+    ordersTable.textContent = '';
+    
+    orders.forEach((order, i) => {
+      //console.log('order, i: ', order, i);
+          
+      ordersTable.innerHTML += `
+      <tr class="order" data-number-order="${i}">
+        <td>${i+1}</td> 
+        <td>${order.description}</td> 
+        <td class="${order.currency}"></td> 
+        <td>${order.deadline}</td> 
+      </tr>`;
+
+    });
+    
+  };
+
+  const openModal = (numberOrder) => {
+    const order = orders[numberOrder];
+    const modal = order.active ? modalOrderActive : modalOrder;
+
+    const 
+          titleBock = document.querySelector('.modal-title'),
+          firstNameBlock = document.querySelector('.firstName'),
+          emailBlock = document.querySelector('.email'),
+          descriptionBlock = document.querySelector('.description'),
+          deadlineBlock = document.querySelector('.deadline'),
+          currencyBlock = document.querySelector('.currency_img'),
+          countBlock = document.querySelector('.count'),
+          phoneBlock = document.querySelector('.phone');
+          
+    
+    titleBock.textContent = order.title;
+    firstNameBlock.textContent = order.firstName;
+    emailBlock.textContent = order.email;
+    descriptionBlock.textContent = order.description;
+    deadlineBlock.textContent = order.deadline;
+    currencyBlock.classList.add(order.currency);
+    countBlock.textContent = order.amount;
+    phoneBlock.href = "tel:"+order.phone;
+    
+
+
+
+
+
+
+    
+
+
+    modal.style.display = 'block';
+
+
+
+  };
+
+  formClose.addEventListener('click', () => {
+    modalOrder.style.display = 'none';
+    modalOrderActive.style.display = 'none';
+  });
+
+  ordersTable.addEventListener('click', (event) => {
+    const target = event.target;
+    //console.log('target: ', target);
+    const targetOrder = target.closest('.order');
+
+    if(targetOrder){
+      openModal(targetOrder.dataset.numberOrder);
+    }
+
+    
+
+
+
+  });
 
   customer.addEventListener('click', () => {
     blockFreelancer.style.display = 'none';
@@ -21,6 +102,7 @@ document.addEventListener('DOMContentLoaded', () => {
   freelancer.addEventListener('click', () => {
     blockCustomer.style.display = 'none';
     blockChoice.style.display = 'none';
+    renderOrders();
     blockFreelancer.style.display = 'block';
     btnExit.style.display = 'block';
     
@@ -40,7 +122,9 @@ document.addEventListener('DOMContentLoaded', () => {
     event.preventDefault();
     
     const obj = {};
-    /* for (const elem of formCustomer.elements){
+    /* 
+    //вариант фильтрации с циклом "for of"
+    for (const elem of formCustomer.elements){
       
       if((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
         (elem.type === 'radio' && elem.checked) ||
@@ -52,11 +136,13 @@ document.addEventListener('DOMContentLoaded', () => {
       }
     }; */
 
+    //вариант фильтрации методом "filter()" и перебором массива 
     const elements = [...formCustomer.elements]
-      .filter(elem => 
-        ((elem.tagName === 'INPUT' && elem.type !== 'radio') ||
+      .filter(elem => (
+        (elem.tagName === 'INPUT' && elem.type !== 'radio') ||
         (elem.type === 'radio' && elem.checked) ||
-        elem.tagName === 'TEXTAREA'));
+        elem.tagName === 'TEXTAREA'
+        ));
     
     elements.forEach((item) => {
       obj[item.name] = item.value;
@@ -72,7 +158,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   });
 
+  
 
-
-  //console.log(customer, freelancer);
 });
